cc3832e39f9fd596f5e75aca71e0c4c4a345a7d7
ww345ww@gmail.com <ww345ww@gmail.com@165d85d3-996a-6913-145b-ba3ad46295f9>
2011-09-14 12:05:08 +0000

    for stack layout

--- trunk/llvm-2.8/include/llvm/CodeGen/AccessAnalysis.h
+++ trunk/llvm-2.8/include/llvm/CodeGen/AccessAnalysis.h
@@ -12,7 +12,7 @@ using namespace std;
 #include "llvm/CodeGen/MachineFunction.h"
 using namespace llvm;
 
-#define CASCH_LINE_SIZE	128
+#define CASCH_LINE_SIZE	64
 #define ASSOCIATIVITY	8
 #define NUM_OF_SETS	1024
 #define	CASCH_CAPACITY	(2 << 20)
@@ -25,17 +25,17 @@ namespace llvm
 
 
 //namespace StackLayout
-//{	
+//{
 	//typedef long long int64_t;
 	// data structure for storage
 	struct AccessRecord
 	{
 		int m_nID;
 		double m_dCount;
-		
+
 	};
-	
-	
+
+
 	class AccessEdge
 	{
 		public:
@@ -46,7 +46,8 @@ namespace llvm
 		int nWR;
 		int nWW;
 		double dWeight;
-		
+		double dCrossWeight;
+
 	public:
 		AccessEdge(int prev, int next)
 		{
@@ -54,10 +55,10 @@ namespace llvm
 			pNext = next;
 			nRR = nRW = nWR = nWW = 0;
 			dWeight = 0.0;
-			
+            dCrossWeight = 0.0;
 		}
 	};
-	
+
 	class CCacheBlock
 	{
 	public:
@@ -65,14 +66,15 @@ namespace llvm
 		//int m_nLeft;
 		int m_nOffset;
 		std::map<int, int> m_hOff2Obj;
-		
-		CCacheBlock(int nID, int offset) {m_nID = 0; m_nOffset = 0;}
-		
+		std::set<int> m_Objs;
+
+		CCacheBlock(int nID, int offset) {m_nID = nID; m_nOffset = offset;}
+
 	};
 	struct EdgeCmp
 	{
-		
-		bool operator () ( AccessEdge *first, AccessEdge *second) 
+
+		bool operator () ( AccessEdge *first, AccessEdge *second)
 		{
 			if( first->dWeight > second->dWeight)
 				return true;
@@ -80,29 +82,32 @@ namespace llvm
 		}
 	};
 	struct RecordCmp
-	{		
-		bool operator () ( AccessRecord *first, AccessRecord *second) 
+	{
+		bool operator () ( AccessRecord *first, AccessRecord *second)
 		{
 			if( first->m_dCount < second->m_dCount)
 				return true;
 			return false;
 		}
 	};
-	
+
 	extern std::map<const Function *, std::map<int, std::map<int, AccessEdge *>  > > hAccRecord; // for computing
 	extern std::map<const Function *, std::map<int, std::set<AccessEdge *, EdgeCmp> > > hAccList;   // for storage
 	extern std::map<const Function *, std::map<int, std::map<int, double> > > hWeightGraph;
-	
+
 	extern std::map<const Function *, std::set<AccessRecord *, RecordCmp> > AccessCount; 		// For access frequency
-	
+
 	extern std::map<Function *, map<int, int64_t> > StackLayout;		// For stack layout
-	
+
 	bool AccessAnalysis(llvm::MachineFunction &mf);
 	bool PackStack(MachineFunction &mf, int64_t &Offset, RegScavenger *RS, int min, int max, SmallSet<int, 16> &);
-	int CacheAlloc(MachineFunction &mf, std::map<int, CCacheBlock *> &hAlloc, int nOffset, int nPrev);
-	int UpdateGraph(std::map<int, std::map<int, double> > &graph, CCacheBlock *pBlock, int nIndex);
-	int AssignOffset(std::list<CCacheBlock *> &Block_list, int nOffset, MachineFunction &mf, bool StackGrowsDown);
+	int CacheOffset(MachineFunction &mf, std::map<int, CCacheBlock *> &hAlloc, int nOffset, int nPrev);
+	int UpdateGraph(std::map<int, std::map<int, double> > &graph, std::map<int, std::set<int> > &removedEdge, CCacheBlock *pBlock, int nIndex);
+	int AssignOffset(std::list<CCacheBlock *> &Block_list, int nOffset, int nOriOff, MachineFunction &mf, bool StackGrowsDown);
 	int DumpGraph(std::map<int, std::map<int, double> > &graph);
+
+	int UpdateEdge(std::map<int, std::map<int, AccessEdge *> > &AccEdge, int nFirst, int nSec, bool bFirst, bool bSec, double dFreq);
+    double GetFrequency(const MachineBasicBlock *mbb,const Function *fn);
 //}
 
 #endif
