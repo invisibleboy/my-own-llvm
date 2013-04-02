90461206693455dc258e975021bedf8e13d8d307
ww345ww <ww345ww@165d85d3-996a-6913-145b-ba3ad46295f9>
2013-04-02 02:23:13 +0000

    A stable version used to evaluate the volatile STT-RAM cache

--- llvm-2.8/ShellScript/simulate.sh
+++ llvm-2.8/ShellScript/simulate.sh
@@ -6,11 +6,41 @@ PIN=$PIN_PATH"pin"
 #OUTPUT=$APP".hybrid"
 
 APP=$1
-PIN_TOOL=$PIN_PATH"source/tools/SimpleExamples/obj-intel64/qaliTrace.so"
-OUTPUT=$APP.trace
+#PIN_TOOL=$PIN_PATH"source/tools/SimpleExamples/obj-intel64/qaliTrace.so"
+#OUTPUT=$APP.trace
 
+#--SymbolTrace
 #PIN_TOOL=$PIN_PATH"source/tools/SimpleExamples/obj-intel64/SymbolTrace.so"
-#OUTPUT=$APP.baseStat
+#SCHEME=0
+#ITRACE=$APP.trace
+#OUTPUT=$APP"_"$SCHEME.statBase0
+#OGRAPH=$APP"_"$SCHEME.graph0
+
+
+PIN_TOOL=$PIN_PATH"source/tools/SimpleExamples/obj-intel64/volTrace2ilp.so"
+PIN_TOOL_OPT=$PIN_PATH"source/tools/SimpleExamples/obj-intel64/SymbolTraceOpti.so"
+ALLOC=/home/qali/ShellScript/allocate
+
+SWITCH=ilp1
+
+SCHEME=$2
+BLOCK_SIZE=$3
+CACHE_SIZE=$4
+
+#--volTrace2ilp
+ITRACE=$APP.trace
+OUTPUT=$APP"_"$SCHEME"_"$BLOCK_SIZE"_"$CACHE_SIZE.statBase
+OTRACE=$APP"_"$SCHEME"_"$BLOCK_SIZE"_"$CACHE_SIZE.traceILP
+OGRAPH_ILP=$APP"_"$SCHEME"_"$BLOCK_SIZE"_"$CACHE_SIZE.graphILP
+
+#--allocate
+
+DATAMAP="alloc_"$SCHEME"_"$BLOCK_SIZE"_"$CACHE_SIZE
+
+#--SymbolTraceOpti
+OUTPUT_OPT=$APP"_"$SCHEME"_"$BLOCK_SIZE"_"$CACHE_SIZE.statOpt
+OUTPUT_OPT_ILP=$APP"_"$SCHEME"_"$BLOCK_SIZE"_"$CACHE_SIZE.statOptIlp
+
 
 #PIN_TOOL=$PIN_PATH"source/tools/SimpleExamples/obj-intel64/SymbolTraceOpti.so"
 #OUTPUT=$APP.optiStat
@@ -27,8 +57,33 @@ OUTPUT=$APP.trace
 	##echo "$PIN -t $PIN_TOOL -b 32 -o $OUTPUT -- ./$APP >log"
 	#$PIN -t $PIN_TOOL -b 32 -o $OUTPUT -- ./$APP >log	
 	
-	echo "$PIN -t $PIN_TOOL -o $OUTPUT -- ./$APP >log"
-	$PIN -t $PIN_TOOL -o $OUTPUT -- ./$APP >log	
+	#--SymbolTrace
+	#echo "$PIN -t $PIN_TOOL -hw $SCHEME -it $ITRACE -og $OGRAPH -o $OUTPUT -- ./$APP >log"
+	#$PIN -t $PIN_TOOL -hw $SCHEME -it $ITRACE -og $OGRAPH -o $OUTPUT -- ./$APP >log	
+
+	# from trace to graph as well as base result
+	#volTrace2ilp
+	echo "$PIN -t $PIN_TOOL -hw $SCHEME -it $ITRACE -b $BLOCK_SIZE -c $CACHE_SIZE -ot $OTRACE -og $OGRAPH_ILP -o $OUTPUT -- ./$APP >log"
+	$PIN -t $PIN_TOOL -hw $SCHEME -it $ITRACE -b $BLOCK_SIZE -c $CACHE_SIZE -ot $OTRACE -og $OGRAPH_ILP -o $OUTPUT -- ./$APP >log
+
+	
+	if [ $SWITCH = "ilp" ]; then		
+		#from graph to allocation.result
+		echo "$ALLOC $SCHEME $BLOCK_SIZE $CACHE_SIZE 1 $OTRACE"_funcs" >> log"
+		$ALLOC $SCHEME $BLOCK_SIZE $CACHE_SIZE 1 $OTRACE"_funcs" >> log
+
+		#from allocation.result to optiStat
+		echo "$PIN -t $PIN_TOOL_OPT -hw $SCHEME -it $ITRACE -id $DATAMAP"_ilp.txt" -b $BLOCK_SIZE -c $CACHE_SIZE -o $OUTPUT_OPT_ILP  -- ./$APP >>log"
+		$PIN -t $PIN_TOOL_OPT -hw $SCHEME -it $ITRACE -id $DATAMAP"_ilp.txt" -b $BLOCK_SIZE -c $CACHE_SIZE -o $OUTPUT_OPT_ILP  -- ./$APP >>log
+	else
+		#from trace ILP to ILP allocation results 
+		echo "$ALLOC $SCHEME $BLOCK_SIZE $CACHE_SIZE 0 $OGRAPH_ILP >> log"
+		$ALLOC $SCHEME $BLOCK_SIZE $CACHE_SIZE 0 $OGRAPH_ILP >> log
+
+		#from allocation.result to optiStat
+		echo "$PIN -t $PIN_TOOL_OPT -hw $SCHEME -it $ITRACE -id $DATAMAP".txt" -b $BLOCK_SIZE -c $CACHE_SIZE -o $OUTPUT_OPT  -- ./$APP >>log"
+		$PIN -t $PIN_TOOL_OPT -hw $SCHEME -it $ITRACE -id $DATAMAP".txt" -b $BLOCK_SIZE -c $CACHE_SIZE -o $OUTPUT_OPT  -- ./$APP >>log	
+	fi
 
 	#echo "$PIN -t $PIN_TOOL -it $APP.trace -og $APP.graph $OUTPUT -- ./$APP >log"
 	#$PIN -t $PIN_TOOL -it $APP.trace -og $APP.graph -o $OUTPUT -- ./$APP >log
